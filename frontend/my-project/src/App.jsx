ff8e502b0a5aa52f6f4de801f7e066b67dca4b05
dev-rfgul <rfgul587@gmail.com>
2024-12-17 16:14:29 +0500

    added images for carousel

--- frontend/my-project/src/App.jsx
+++ frontend/my-project/src/App.jsx
@@ -9,7 +9,7 @@ const App = () => {
     <>
       <div className='mx-16 '>
         <Navbar />
-        <Home />
+     
         <Carousel/>
       </div>
     </>
--- frontend/my-project/src/components/Carousel.jsx
+++ frontend/my-project/src/components/Carousel.jsx
@@ -10,10 +10,10 @@ const Carousel = () => {
         centerMode: true,
         infinite: true,
         centerPadding: "60px",
-        slidesToShow: 6,
+        slidesToShow: 1,
         speed: 500,
         autoplay: true,
-        autoplaySpeed: 2000, // 2 seconds
+        autoplaySpeed: 1000, // 2 seconds
         responsive: [
             {
                 breakpoint: 1024,
@@ -44,61 +44,41 @@ const Carousel = () => {
             age: 20,
             review:
                 "Hello I am Fahad, and I want to say that styling is fun and I love it.",
-            img: "./images/product-1.jpg",
+            img: "./images/carousel-image-1.png",
         },
         {
             name: "B",
             age: 25,
             review:
                 "GST Registration was never this easy. Register your sales tax with Befiler Now.",
-            img: "./images/product-2.jpg",
+            img: "./images/carousel-image-2.jpeg",
         },
         {
             name: "C",
             age: 30,
             review:
                 "GST Registration was never this easy. Register your sales tax with Befiler Now.",
-            img: "./images/product-3.jpg",
+            img: "./images/carousel-image-3.jpeg",
         },
-        {
-            name: "D",
-            age: 35,
-            review:
-                "GST Registration was never this easy. Register your sales tax with Befiler Now.",
-            img: "./images/product-2.jpg",
-        },
+  
     ];
 
     return (
-        <div className="w-3/4 m-auto">
+        <div className="w-full m-auto overflow-hidden">
             <div className="mt-20">
                 <Slider {...settings}>
                     {data.map((d, index) => (
                         <div
                             key={index}
-                            className="m-5 bg-white h-[450px] text-black rounded-xl max-w-sm overflow-hidden shadow-lg transition duration-300 transform hover:scale-105"
-                        >
-                            <div className="h-56 rounded-t-xl bg-indigo-500 flex justify-center items-center">
-                                <img
-                                    src={d.img}
-                                    className="w-44 h-44 rounded-full object-cover"
-                                    alt={d.name}
-                                />
-                            </div>
-                            <div className="flex flex-col justify-center items-center p-5">
-                                <p className="text-lg font-semibold">{d.name}</p>
-                                <p className="text-center">Age: {d.age}</p>
-                                <p className="text-gray-800">{d.review}</p>
-                                <button className="mt-2 px-4 py-2 bg-blue-500 text-white rounded">
-                                    Read More
-                                </button>
-                            </div>
-                        </div>
+                            className="w-full h-[500px]  bg-repeat bg-center"
+                        ></div>
                     ))}
                 </Slider>
             </div>
         </div>
     );
+    
+    
 };
 
 export default Carousel;
