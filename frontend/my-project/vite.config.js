7a80ff55c1cf6ab446971cca21fa52778dad2626
dev-rfgul <rfgul587@gmail.com>
2024-12-18 03:45:31 +0500

    trying to deploy the project but error is arising due to react carousel

--- frontend/my-project/vite.config.js
+++ frontend/my-project/vite.config.js
@@ -1,7 +1,24 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  resolve: {
+    alias: {
+      // Resolve node_modules path if necessary
+      'slick-carousel/slick/slick.css': 'slick-carousel/slick/slick.css',
+      'slick-carousel/slick/slick-theme.css': 'slick-carousel/slick/slick-theme.css',
+    },
+  },
+  optimizeDeps: {
+    include: ['react-slick', 'slick-carousel'],
+  },
+  build: {
+    rollupOptions: {
+      external: [
+        'slick-carousel/slick/slick.css',
+        'slick-carousel/slick/slick-theme.css',
+      ],
+    },
+  },
+});
