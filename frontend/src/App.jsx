b5c4988da140ca2b20c056651eca354ab497fd2e
dev-rfgul <rfgul587@gmail.com>
2025-01-30 02:14:15 +0500

    the deployment issue resolved, now verifying wether issue is resovled or not by formating the app.jsx page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -13,12 +13,12 @@ const App = () => {
     <>
       <div>
         <Navbar />
-        <FeatureSection />
         <Carousel />
         <Heading heading={"Explore"} subHeading={"Explore our wide range of products"} />
         <Explore />
         <Heading heading={"Featured Products"} subHeading={"Check out our Featured Products"} />
         <FeaturedProducts />
+        <FeatureSection />
         {/* <ProductDisplay /> */}
         <Footer />
 
