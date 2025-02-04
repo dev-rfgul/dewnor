8f2e252e83d04926200bea15ed31228fce056741
dev-rfgul <rfgul587@gmail.com>
2025-02-04 22:22:35 +0500

    added user and product model and they are almost final but few changes can be made on further working

--- backend/index.js
+++ backend/index.js
@@ -1,16 +1,16 @@
-const express = require('express');
-const dotenv = require('dotenv')
-
+import express from 'express';
+import dotenv from 'dotenv'
 const app = express();
 dotenv.config();
 
 
 app.get('/', (req, res) => {
-    res.send("hello world the backend is working")
+    res.send(" the backend is working")
 })
 
 
 
+
 const port = process.env.LOCALHOST
-console.log("the backend is running on port :",port)
+console.log(" hello the backend is running on port :", port)
 app.listen(port)
---
+++ backend/models/product.model.js
@@ -0,0 +1,33 @@
+import mongoose from 'mongoose'
+
+const ProductSchema = new mongoose.model({
+    name: {
+        type: String,
+        unique: true,
+        required: true,
+    },
+    description: {
+        type: String,
+        required: true,
+    },
+    price: {
+        type: int32,
+        required: true,
+    },
+    stock: {
+        type: int32,
+        required: true,
+    },
+    color: {
+        type: String,
+    },
+    imgs: {
+        type: String,
+        required: true,
+    },
+    size: {
+        type: String,
+    }
+})
+
+export const Products = mongoose.model('Products', ProductSchema);
